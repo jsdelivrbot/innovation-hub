aab378001353ba185f23f9ec35c0a54923ba264b
FelixGanz <felix.ganz@epitech.eu>
2017-07-10 17:35:41 +0200

    Test module Database

---
+++ app/database.js
@@ -0,0 +1,55 @@
+var Sequelize = require('sequelize');
+
+function Database()
+{
+    this.name = process.env.DB_NAME;
+    this.host = process.env.DB_HOST;
+    this.user = process.env.DB_USER;
+    this.pass = process.env.DB_PASSWORD;
+
+    this.sequelize = new Sequelize(this.name, this.user, this.pass,
+					 {
+					     host: this.host,
+					     dialect: 'postgres',
+					     
+					     pool: {
+						 max: 5,
+						 min: 0,
+						 idle: 10000
+					     },
+					     console.log('Node app is trying to connect to ' + process.env.DB_NAME + " on host " + process.env.DB_HOST);
+					 });
+    this.getSequelize = function()
+    {
+	return this.sequelize;
+    };
+
+    this.connect = function()
+    {
+	sequelize.authenticate().then(() => {
+	    console.log('Connection has been established successfully.');
+	})
+	    .catch(err => {
+		console.error('Unable to connect to the database:', err);
+	    });	
+    }
+}
+
+const User = sequelize.define('user', {
+    firstName: {
+	type: Sequelize.STRING
+    },
+    lastName: {
+	type: Sequelize.STRING
+    }
+});
+
+User.sync({force: true}).then(() => {
+    // Table created
+    return User.create({
+	firstName: 'John',
+	lastName: 'Hancock'
+    });
+});
+
+module.exports = new Database;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var Sequelize = require('sequelize');
 var express = require('express');
 var app = express();
+var Database = require('app/database');
 
 // var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
@@ -19,49 +19,13 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-
-    pool: {
-	max: 5,
-	min: 0,
-	idle: 10000
-    },
-});
-console.log('Node app is trying to connect to ' + process.env.DB_NAME + " on host " + process.env.DB_HOST);
-
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-})
-    .catch(err => {
-	console.error('Unable to connect to the database:', err);
-    });
-
-const User = sequelize.define('user', {
-    firstName: {
-	type: Sequelize.STRING
-    },
-    lastName: {
-	type: Sequelize.STRING
-    }
-});
-
-User.sync({force: true}).then(() => {
-    // Table created
-    return User.create({
-	firstName: 'John',
-	lastName: 'Hancock'
-    });
-});
-
-
 app.get('/db', function(request, response)
 	{
 	    // force: true will drop the table if it already exists
-	    User.findAll().then(users => {
-		console.log(users)
-	    })
+	    Database.connect();
+	    // User.findAll().then(users => {
+	    // 	console.log(users)
+	    // })
 	    
 	    response.render('pages/db');
 	});
