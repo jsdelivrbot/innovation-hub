53eadf771a063f83f286932f455cc88c5b6cb347
FelixGanz <felix.ganz@epitech.eu>
2017-07-11 19:28:43 +0200

    encore une autre otpion

--- app/database.js
+++ app/database.js
@@ -57,19 +57,20 @@ function Database()
 	console.log(this.models);
 	console.log(this.models['users']);
 	var sequelize = this.sequelize;
-	this.models.keys().forEach(function(modelName)
-				   {
-				       console.log('Fetching model ' + model);
-				       model.model = sequelize.define(model, model.hash);
-				       model.model.sync({force: true}).then(() => {
-    	    				   // Table created
-    	    				   return model.model.create({
-    	    				       firstName: 'John',
-    	    				       lastName: 'Hancock'
-    	    				   });
-				       });
-				       console.log('Model is ' + model.model);
-				   });
+	for (var key in this.models)
+	{
+	    var model = this.models[key];
+	    console.log('Fetching model ' + model);
+	    model.model = sequelize.define(model, model.hash);
+	    model.model.sync({force: true}).then(() => {
+    	    	// Table created
+    	    	return model.model.create({
+    	    	    firstName: 'John',
+    	    	    lastName: 'Hancock'
+    	    	});
+	    });
+	    console.log('Model is ' + model.model);
+	};
     }
 
     
