f12ab08499f3109d95f13ba03cb7d534a3ed3c58
FelixGanz <felix.ganz@epitech.eu>
2017-07-15 22:48:44 +0200

    ok

--- app/models.js
+++ app/models.js
@@ -10,6 +10,11 @@ Models['user'] =
 	    lastName: { type: Sequelize.STRING, defaultValue: 'LastName' },
 	    promo: { type: Sequelize.INTEGER, defaultvalue: 2020 },
 	    imgurl: { type: Sequelize.STRING, defaultvalue: 'https://vignette2.wikia.nocookie.net/mafiagame/images/2/23/Unknown_Person.png/revision/latest?cb=20151119092211'},
+	    there: { type: Sequelize.BOOLEAN, defaultvalue: false},
+	    spec: { type: Sequelize.STRING, defaultvalue: 'Undefined'},
+	    title: { type: Sequelize.STRING, defaultvalue: 'Peon'},
+	    music: { type: Sequelize.STRING, defaultvalue: 'Undefined'},
+	    rank: { type: Sequelize.INTEGER, defaultvalue: -1},
 	},
     };
 
@@ -18,12 +23,22 @@ Models['hubData'] =
 	model: null,
 	hash: {
 	    name: { type: Sequelize.STRING, defaultValue: 'DefaultName' },
-	    origin: { type: Sequelize.STRING, defaultValue: 'Innovation Hub' },
-	    unit: { type: Sequelize.STRING, defaultValue: 'Undefined' },
 	    category: { type: Sequelize.STRING, defaultValue: 'Undefined' },
-	    value: { type: Sequelize.INTEGER, defaultValue: -1 },
-	    svalue: { type: Sequelize.STRING, defaultValue: 'Undefined' },
+	    room: { type: Sequelize.STRING, defaultValue: 'Innovation Hub' },
+	    boxId: { type: Sequelize.INTEGER, defaultValue: 0},
+	    value: { type: Sequelize.JSON, defaultValue: -1 },
 	},
+    }
+
+Models['hubBox'] =
+    {
+	model:null,
+	hash: {
+	    realId: {type: Sequelize.INTEGER, defaultvalue :0},
+	    creators: {type: Sequelize.STRING, defaultvalue: 'Undefined'},
+	    category: {type: Sequelize.STRING, defaultvalue: 'Undefined'},
+	    room: {type: Sequelize.STRING, defaultvalue: 'Innovation Hub'},
+	}
     }
 
 module.exports = Models;
