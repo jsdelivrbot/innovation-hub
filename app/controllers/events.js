eef8c2d5586ac4145a3eafdaa52b68f0d9407870
FelixGanz <felix.ganz@epitech.eu>
2017-07-17 13:25:38 +0200

    test online

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -8,118 +8,23 @@ var googleAuth = require('google-auth-library');
 function EventsController(database)
 {
 
-    // If modifying these scopes, delete your previously saved credentials
-    // at ~/.credentials/calendar-nodejs-quickstart.json
-    var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-    var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-		     process.env.USERPROFILE) + '/.credentials/';
-    var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
-
     this.API = function(callback, client, date, end)
     {
-	// Load client secrets from a local file.
-	fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+	var key = require('../../googleapi-privatekey.json');
+	var jwtClient = new google.auth.JWT(
+	    key.client_email,
+	    null,
+	    key.private_key,
+	    ['https://www.googleapis.com/auth/calendar.readonly'],
+	    null
+	);
+	
+	jwtClient.authorize(function (err, tokens) {
 	    if (err) {
-		console.log('Error loading client secret file: ' + err);
+		console.log('Auth error : ' + err);
 		return;
 	    }
-	    // Authorize a client with the loaded credentials, then call the
-	    // Google Calendar API.
-	    console.log('Success');
-	    authorize(JSON.parse(content), callback);
 	});
-
-	/**
-	 * Create an OAuth2 client with the given credentials, and then execute the
-	 * given callback function.
-	 *
-	 * @param {Object} credentials The authorization client credentials.
-	 * @param {function} callback The callback to call with the authorized client.
-	 */
-	function authorize(credentials, callback) {
-	    var clientSecret = credentials.installed.client_secret;
-	    var clientId = credentials.installed.client_id;
-	    var redirectUrl = credentials.installed.redirect_uris[0];
-	    var auth = new googleAuth();
-	    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-	    google.options({
-		auth: oauth2Client
-	    });
-
-	    var url = oauth2Client.generateAuthUrl({
-		// 'online' (default) or 'offline' (gets refresh_token)
-		access_type: 'offline',
-
-		// If you only need one scope you can pass it as a string
-		scope: SCOPES,
-
-		// Optional property that passes state parameters to redirect URI
-		// state: { foo: 'bar' }
-	    });
-
-	    oauth2Client.setCredentials('4/78iKJb8j_GegkrCEgivyINcSta5BQZzkkYG1QSNcn1U');
-	    
-	    // Check if we have previously stored a token.
-	    fs.readFile(TOKEN_PATH, function(err, token) {
-		if (err) {
-		    getNewToken(oauth2Client, callback);
-		} else {
-		    oauth2Client.credentials = JSON.parse(token);
-		    if (client)
-			callback(oauth2Client, client, date, end);
-		}
-	    });
-	}
-
-	/**
-	 * Get and store new token after prompting for user authorization, and then
-	 * execute the given callback with the authorized OAuth2 client.
-	 *
-	 * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
-	 * @param {getEventsCallback} callback The callback to call with the authorized
-	 *     client.
-	 */
-	function getNewToken(oauth2Client, callback) {
-	    var authUrl = oauth2Client.generateAuthUrl({
-		access_type: 'offline',
-		scope: SCOPES
-	    });
-	    console.log('Authorize this app by visiting this url: ', authUrl);
-	    var rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	    });
-	    rl.question('Enter the code from that page here: ', function(code) {
-		rl.close();
-		oauth2Client.getToken(code, function(err, token) {
-		    if (err) {
-			console.log('Error while trying to retrieve access token', err);
-			return;
-		    }
-		    oauth2Client.credentials = token;
-		    storeToken(token);
-		    callback(oauth2Client);
-		});
-	    });
-	}
-
-	/**
-	 * Store token to disk be used in later program executions.
-	 *
-	 * @param {Object} token The token to store to disk.
-	 */
-	function storeToken(token) {
-	    try {
-		fs.mkdirSync(TOKEN_DIR);
-	    } catch (err) {
-		if (err.code != 'EEXIST') {
-		    throw err;
-		}
-	    }
-	    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-	    console.log('Token stored to ' + TOKEN_PATH);
-	}
     }
 
     /**
