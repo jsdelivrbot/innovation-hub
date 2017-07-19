5227b32b443074008ab4b4bf321bf47bff0e9044
FelixGanz <felix.ganz@epitech.eu>
2017-07-19 22:48:16 +0200

    no sync

--- test.py
+++ test.py
@@ -1,30 +1,22 @@
 import json
 import requests
 
-# host = 'https://innovation-hub.herokuapp.com/'
-host = 'http://localhost:6000/'
-
-# category: { type: Sequelize.STRING, defaultValue: 'Undefined' },
-# room: { type: Sequelize.STRING, defaultValue: 'Innovation Hub' },
-# boxId: { type: Sequelize.INTEGER, defaultValue: 0},
-# value: { type: Sequelize.INTEGER, defaultValue: -1 },
-# unit: { type: Sequelize.STRING, defaultValue: 'Undefined'},
-# img: { type: Sequelize.STRING, defaultValue: 'Undefined'},
+host = 'https://innovation-hub.herokuapp.com/'
 
 data = {
     'category' : 'Temperature',
-    'room': 'Hallway',
+    'room': 'Lazy Studio',
     'boxId' : '1',
-    'value' : '18',
-    'unit' : 'Celsius',
+    'value' : '520',
+    'unit' : 'Lumen',
     'img' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGnXCC-4JueYCdreZaMc_Toid9mfVTEwpLrG__9Iz7qZV00t-P',
 }
 
-r = requests.post(host + 'hubData/create', data);
-print('Create');
-print(r.text);
-# r = requests.get(host + 'hubData/');
+# r = requests.post(host + 'hubData/create', data);
+# print('Create');
 # print(r.text);
+r = requests.get(host + 'hubData/');
+print(r.text);
 # datas = json.loads(r.text);
 # for data in datas:
 #     print(data);
